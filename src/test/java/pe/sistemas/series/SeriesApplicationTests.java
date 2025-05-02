9ccd7250e7a9b919e7b40cb0c2d0fbf3ee13382b
EDERANDER <ea.quispe@est.unap.edu.pe>
2025-05-02 17:17:54 -0500

    creando proyecto de persistencia de datos de una api de series

---
+++ src/test/java/pe/sistemas/series/SeriesApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.sistemas.series;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
